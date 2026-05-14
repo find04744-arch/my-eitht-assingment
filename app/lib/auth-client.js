2c5564a2ba930cfb3636bee5269677690a99dcb0
find04744-arch <find04744@gmail.com>
2026-05-14 12:27:20 +0600

    this is last

--- app/lib/auth-client.js
+++ app/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    // এখানে আপনার বেইজ ইউআরএল দিন (লোকাল হোস্টের জন্য এটিই থাকবে)
+ 
     baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000" 
 });
---
+++ next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ["better-auth"],
+};
+
+export default nextConfig;
